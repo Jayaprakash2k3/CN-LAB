3ba5d8b9bef17c6b85934c4bdd86bc1b22f251cd
Jayaprakash M <jayaprakash2k3@gmail.com>
2022-12-06 06:43:07 +0530

    Sliding Window

---
+++ Python/Sliding Window/client.py
@@ -0,0 +1,18 @@
+#some Minor Correction will be Done in the afternoon
+import socket
+c=socket.socket()
+PORT=12345
+win=int(input("Enter the Window Size:"))
+c.connect(('localhost',PORT))
+while True:
+    msg=c.recv(1).decode()
+    if(msg=="#"):
+        c.close()
+        break
+    print(f'{msg} has been received')
+    if(msg!=''):
+        c.send("1".encode())
+    else:
+        c.send("0".encode())
+    print(f'ack for {msg} has been sent')
+
---
+++ Python/Sliding Window/server.py
@@ -0,0 +1,35 @@
+#some Minor Correction will be Done in the afternoon
+# Reamining Codes will be uploaded in the afternoon
+import socket
+s=socket.socket()
+PORT=12345
+s.bind(('',PORT))
+s.listen(5)
+data=input("Enter the message to Send:")
+win=int(input("Enter the Window Size:"))
+count=0
+sent=0
+while True:
+    c,a=s.accept()
+    print("connected to ",a)
+    while sent!=len(data):
+        while count!=win and sent!=len(data):
+            c.send(data[sent].encode())
+            count+=1
+            sent+=1
+            print(f"{data[sent-1]} has been sent")
+        if(sent!=len(data)-1):
+            ack=c.recv(1).decode()
+        else:
+            ack=c.recv(win).decode()
+        for k in ack:
+            if(k=='1'):
+                print("Acknowledement has been received")
+                count-=1
+            else:
+                sent-=1
+                count-=1
+            
+    c.send("#".encode())        # End of Packet Indicator
+
+    c.close()
